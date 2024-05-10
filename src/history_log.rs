fc43a53d25b34488a91a6d82c7a15b88e62fb8e5
Ethan Lew <elew@galois.com>
2024-05-10 12:18:27 +0800

    add line numbers to the construction from git blame

--- src/history_log.rs
+++ src/history_log.rs
@@ -1,19 +1,24 @@
 use chrono::{NaiveDateTime, DateTime, Utc};
-use git2::{BlameOptions, Repository, Time};
+use git2::{BlameOptions, Repository};
 use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
+
+pub struct Line {
+    line_no: usize,
+    line: String,
+}
 
 pub struct Message {
     author_email: String,
     date: DateTime<Utc>,
     commit_id: String,
     message: String,
-    lines: Vec<String>,
+    lines: Vec<Line>,
 }
 
 pub struct HistoryLog {
-    entries: HashMap<(String, String), Vec<Message>>, // (Date as YYYY-MM-DD, Author Email) -> Vec of messages
+    entries: HashMap<(String, String), Vec<Message>>,
 }
 
 impl HistoryLog {
@@ -38,33 +43,31 @@ impl HistoryLog {
         let mut blame_options = BlameOptions::new();
         let full_path = Path::new(repo_path).join(path);
         let file_contents = fs::read_to_string(full_path).expect("unable to read string");
+        let file_lines: Vec<String> = file_contents.lines().map(String::from).collect();
 
         blame_options.newest_commit(commit.id());
         let blame = repo.blame_file(path, Some(&mut blame_options))?;
 
         let mut log = HistoryLog::new();
-        let mut seen = std::collections::HashSet::new();
 
         for hunk in blame.iter() {
             let commit_id = hunk.final_commit_id();
-            
-            if seen.insert(commit_id) {
-                let commit = repo.find_commit(commit_id)?;
-                let author = commit.author();
-                let time = commit.time();
-                let date = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(time.seconds(), 0), Utc);
-                let line_start = hunk.final_start_line();
-                let line_count = hunk.lines_in_hunk();
-                let lines = file_contents.lines().skip(line_start as usize - 1).take(line_count as usize).map(String::from).collect();
-                let message = Message {
-                    author_email: author.email().unwrap_or("no-email").to_string(),
-                    date,
-                    commit_id: commit_id.to_string(),
-                    message: commit.summary().unwrap_or("No commit message").to_string(),
-                    lines: lines,
-                };
-                log.add_entry(message);
-            }
+           
+            let commit = repo.find_commit(commit_id)?;
+            let author = commit.author();
+            let time = commit.time();
+            let date = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(time.seconds(), 0), Utc);
+            let line_start = hunk.final_start_line();
+            let line_count = hunk.lines_in_hunk();
+            let lines = (line_start..line_start+line_count).map(|i| Line{line_no: i, line: file_lines.get(i).unwrap_or(&String::from("<error>")).to_string()}).collect();
+            let message = Message {
+                author_email: author.email().unwrap_or("<UNKNOWN EMAIL>").to_string(),
+                date,
+                commit_id: commit_id.to_string(),
+                message: commit.summary().unwrap_or("<NO COMMIT MESSAGE>").to_string(),
+                lines: lines,
+            };
+            log.add_entry(message);
         }
 
         Ok(log)
@@ -77,7 +80,7 @@ impl HistoryLog {
             for message in messages {
                 println!("    -- {} ({})", message.message, message.commit_id);
                 for line in &message.lines {
-                    println!("        {}", line);
+                    println!("        {} {}", line.line_no, line.line);
                 }
             }
         }
