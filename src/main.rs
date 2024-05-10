b7e384c15b5546c82f0ea3c75eeb55e10720a492
Ethan Lew <elew@galois.com>
2024-05-10 11:52:35 +0800

    reformat

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,30 @@ async fn main() {
         .version("1.0")
         .author("Ethan Lew <ethanlew16@gmail.com>")
         .about("Automation for generating tedious boilerplate comments.")
-        .arg(Arg::with_name("repo")
-             .short('r')
-             .long("repo")
-             .takes_value(true)
-             .required(true)
-             .help("Directory to Git Repo"))
-        .arg(Arg::with_name("file_path")
-             .short('f')
-             .long("file")
-             .takes_value(true)
-             .required(true)
-             .help("File path within the GitHub repository"))
-        .arg(Arg::with_name("branch")
-             .short('b')
-             .long("branch")
-             .takes_value(true)
-             .required(true)
-             .help("branch or commit"))
+        .arg(
+            Arg::with_name("repo")
+                .short('r')
+                .long("repo")
+                .takes_value(true)
+                .required(true)
+                .help("Directory to Git Repo"),
+        )
+        .arg(
+            Arg::with_name("file_path")
+                .short('f')
+                .long("file")
+                .takes_value(true)
+                .required(true)
+                .help("File path within the GitHub repository"),
+        )
+        .arg(
+            Arg::with_name("branch")
+                .short('b')
+                .long("branch")
+                .takes_value(true)
+                .required(true)
+                .help("branch or commit"),
+        )
         .get_matches();
 
     let file_path = matches.value_of("file_path").unwrap();
@@ -35,8 +41,7 @@ async fn main() {
     match HistoryLog::from_git_blame(repo_path, file_path, branch_name) {
         Ok(log) => {
             log.pretty_print();
-        },
+        }
         Err(e) => eprintln!("Error processing git blame: {}", e),
     }
 }
-
